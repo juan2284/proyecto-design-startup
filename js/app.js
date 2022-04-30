8fe98b4975ae9a09ee396253e371d951f77c37c6
Entropic Cat <86585719+juan2284@users.noreply.github.com>
2022-04-30 00:15:21 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.caracteristicas', { delay: 200, easing: 'ease-in' });
+ScrollReveal().reveal('.fotos', { delay: 200, easing: 'ease-in' });
+ScrollReveal().reveal('.precios', { delay: 200, easing: 'ease-in' });
+ScrollReveal().reveal('.reviews', { delay: 200, easing: 'ease-in' });
